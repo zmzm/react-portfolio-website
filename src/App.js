081e9018b3307e5c4e26190c72a3ea40f48cb831
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-07-18 22:41:25 +0200

    added hoem page, layout styles

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import './App.scss';
+import Home from './components/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import LogogTitle from '../../assets/images/logo-s.png';
+
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi <br /> I&apos;m
+        </h1>
+        <img alt="developer" src={LogogTitle} />
+        lobodan
+        <br />
+        JS developer
+        <Link to="/contact" calssName="flat-butoon">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
