12cd62eb86f09cbc85c6c4c98493ebfcf9e915e6
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-07-26 07:41:44 +0200

    added about page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import './App.scss';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
       </Route>
     </Routes>
   );
